45e1a1aebd43a859d1b84f36873a9b93419b2786
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-10 19:07:27 -0300

    Testes Find By Id Hotel

--- tests/Feature/App/Repositories/HotelRepositoryTest.php
+++ tests/Feature/App/Repositories/HotelRepositoryTest.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Models\Hotel;
 use App\Repositories\Eloquent\HotelEloquentRepository;
 use App\Repositories\Interfaces\HotelRepositoryInterface;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Tests\TestCase;
 
 class HotelRepositoryTest extends TestCase
@@ -22,6 +23,22 @@ class HotelRepositoryTest extends TestCase
         $this->assertDatabaseHas('hotels', [
             'name' => $data['name']
         ]);
+    }
+
+    public function test_find_by_id(): void
+    {
+        $data = Hotel::factory()->create();
+
+        $hotel = $this->repository->findById($data->id);
+
+        $this->assertInstanceOf(Hotel::class, $hotel);
+        $this->assertEquals($hotel->name, $data['name']);
+    }
+
+    public function test_find_by_id_fail(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+        $this->repository->findById(1);
     }
 
     protected function setUp(): void
